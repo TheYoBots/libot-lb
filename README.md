75295385a0177aae31162af6a337f3c7179ede43
TheYoBots <yohaan.nathanjw@gmail.com>
2023-07-14 15:42:51 +0000

    Complete Leaderboard 1. Fixed bot_leaderboard.py 2. Updated all Leaderboards 3. Added Readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # libot-lb
-A leaderboard for all Lichess Bots
+*Based on [lightningbolts/Lichess-Bot-Leaderboards](https://github.com/lightningbolts/Lichess-Bot-Leaderboards)*
+
+A leaderboard for all Lichess Bots. It checks for list of online bots using [Lichess' Bot API](https://lichess.org/api#tag/Bot/operation/apiBotOnline) and creates a list ([available_bots.txt](./available_bots.txt)). Everytime it runs this list is checked to see if any online bots aren't in it and if they aren't, those bots are added to the list. Once that's done, using Lichess' API, the [bots public data](https://lichess.org/api#tag/Users/operation/apiUser) is taken to get the bots rating (This might take a while!). This data is converted into `json` format and is then easily sorted based on rating in each game type (or variant).
+
+## Run
+1. Install dependencies:
+```bash
+pip install -r requirements.txt
+```
+2. Execute python script:
+```bash
+python3 bot_leaderboard.py
+```
--- bot_leaderboard.py
+++ bot_leaderboard.py
@@ -2,6 +2,8 @@ import urllib.request
 import orjson
 import sys
 import lichess.api
+import json
+import os
 
 def types():
     return [
@@ -37,9 +39,13 @@ def get_banned_bots():
         'MustafaYilmazBot'
     ]
 
-def get_user_rating(username, type):
+def get_user_rating(username):
     user = lichess.api.user(username)
-    return [username, user.get('perfs', {}).get(type, {}).get('rating'), user.get('perfs', {}).get(type, {}).get('prov'), user.get('tosViolation')]
+    return {
+        'username': user.get('username'),
+        'perfs': user.get('perfs', {}),
+        'tosViolation': user.get('tosViolation')
+    }
 
 def get_available_bots():
     available_bots = set()
@@ -54,6 +60,7 @@ def get_available_bots():
                 d = orjson.loads(i)
                 if d['username'] not in available_bots:
                     available_bots.append(d['username'])
+                    print(f"Adding {d['username']} to available bots list")
     except Exception as e:
         print(e)
     with open('available_bots.txt', 'w') as f:
@@ -63,25 +70,25 @@ def get_available_bots():
 
 def get_bot_ratings_online(type):
     banned_bots = get_banned_bots()
-    with open('available_bots.txt', 'r') as f:
-        available_bots = []
-        for bot in f.readlines():
-            available_bots.append(bot.strip())
+
+    file_path = os.path.join(os.path.dirname(__file__), 'bot_leaderboard.json')
+    with open(file_path, 'r') as f:
+        bot_ratings = json.load(f)
 
     user_arr = []
+    count = 1
 
-    for bot in available_bots:
-        result = get_user_rating(bot, type)
-        if result is not None:
-            if result[0] not in banned_bots and result[1] is not None and result[2] != True and result[3] != True:
+    for d in bot_ratings:
+        try:
+            result = [d['username'], d['perfs'][type]['rating']]
+            print(f'BOT {result[0]}: {result[1]} in {type}.')
+            if result[0] not in banned_bots or d['perfs'][type]['prov'] != True or d['tosViolation'] != True:
                 user_arr.append(result)
-                print(f'BOT {result[0]}: {result[1]} in {type}')
-
+        except:
+            print(f"BOT {d['username']}: No {type} rating available")
     resulting_arr = sorted(user_arr, key=lambda x: x[1], reverse=True)
-
     with open(get_file_name(type), 'w') as f:
-        count = 1
-        print(f"Rank|Bot|Rating", file=f)
+        print("Rank|Bot|Rating", file=f)
         print("---|---|---", file=f)
         for j in resulting_arr:
             print(f"#{str(count)}|@{j[0]}|{str(j[1])}", file=f)
@@ -89,9 +96,25 @@ def get_bot_ratings_online(type):
 
     print(f"Finished generating leaderboard for {type}")
 
+def get_all_bot_ratings():
+    all_bot_ratings = []
+    with open('available_bots.txt', 'r') as f:
+        available_bots = [bot.strip() for bot in f.readlines()]
+
+    for bot in available_bots:
+        result = get_user_rating(bot)
+        if result is not None:
+            all_bot_ratings.append(result)
+            print(f'Getting rating of BOT {result["username"]}')
+
+    with open('bot_leaderboard.json', 'w') as f:
+        json.dump(all_bot_ratings, f)
+    print("Updated bot_leaderboard.json file.")
+
 if __name__ == "__main__":
     try:
         get_available_bots()
+        get_all_bot_ratings()
         for i in types():
             get_bot_ratings_online(i)
     except KeyboardInterrupt:
