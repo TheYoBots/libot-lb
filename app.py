d7d07696d8071b0dc9dfb9e42eedb2ea832a327d
Yohaan Seth Nathan <73843275+TheYoBots@users.noreply.github.com>
2025-11-23 19:01:56 +0530

    Search for Bot Rank (#27)
    Enter bot's name and get rank in all variants (with rules).

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_file
+import os
 
 app = Flask(__name__)
 
@@ -30,6 +31,30 @@ def bot():
 @app.route('/unrestricted')
 def unrestricted():
     return send_file('html/Unrestricted.html')
+
+@app.route('/bot-rank')
+def botsearch():
+    return send_file('html/BotRank.html')
+
+@app.route('/available_bots.txt')
+def available_bots_file():
+    return send_file('available_bots.txt')
+
+@app.route('/bot_leaderboard/<path:filename>')
+def serve_bot_leaderboard_md(filename):
+    safe_path = os.path.join(os.path.dirname(__file__), 'bot_leaderboard', filename)
+    if os.path.exists(safe_path):
+        return send_file(safe_path)
+    else:
+        return "Not found", 404
+
+@app.route('/js/<path:filename>')
+def serve_js(filename):
+    safe_path = os.path.join(os.path.dirname(__file__), 'js', filename)
+    if os.path.exists(safe_path):
+        return send_file(safe_path)
+    else:
+        return "Not found", 404
 
 @app.route('/bot/<type_name>')
 def bot_type(type_name):
